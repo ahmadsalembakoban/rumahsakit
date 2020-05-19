c9f958ad96ceb66c4dbb18ae0b0e41eafaa42857
unknown <ahmad.salem88@gmail.com>
2020-05-20 06:30:17 +0700

    updated file >> dashboard > index

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     require_once "_config/config.php";
 
     if(isset($_SESSION['user'])){
-        echo "Hello world, <a href=\"auth/logout.php\"> Logout</a>";
+        // echo "Hello world, <a href=\"auth/logout.php\"> Logout</a>";
+        echo "<script>window.location='".base_url('dashboard')."';</script>";
     } else {
         echo "<script>window.location='".base_url('auth/login.php')."';</script>";
     }
