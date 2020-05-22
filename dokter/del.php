529da034b625856b93790187ed8d924e252a36b9
unknown <ahmad.salem88@gmail.com>
2020-05-23 03:11:28 +0700

    step 14: crud dan bulk delete pada data dokter

---
+++ dokter/del.php
@@ -0,0 +1,17 @@
+
+<?php
+require_once "../_config/config.php";
+
+$chk = $_POST['checked'];
+    if(!isset($chk)) {
+        echo "<script>alert('Tidak ada data yang dipillih!'); window.location='data.php';</script>";
+    } else {
+        foreach($chk as $id) {
+            $sql = mysqli_query($conn, "DELETE FROM tb_dokter WHERE id_dokter = '$id'") or die (mysqli_error($conn));
+        }
+        if($sql) {
+            echo "<script>alert('".count($chk)." data berhasil dihapus'); window.location='data.php';</script>";
+        } else {
+            echo "<script>alert('Gagal hapus data, coba lagi');</script>"; 
+        }
+    }  
